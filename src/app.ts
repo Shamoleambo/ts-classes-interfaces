6e7fc05acfc5d15a67a68ab86f89ce09dfd84337
Tiago Gomes <tidgomes@gmail.com>
2023-04-19 13:45:04 -0300

    implement an interface Person

--- src/app.ts
+++ src/app.ts
@@ -1,84 +1,18 @@
-abstract class Department {
-  static fiscalYear = 2023
-  protected employees: string[] = []
+interface Person {
+  name: string
+  age: number
 
-  constructor(protected readonly id: string, public name: string) {}
+  greet(phrase: string): void
+}
 
-  static createEmployee(name: string): {} {
-    return { name }
-  }
+let user1: Person
 
-  abstract describe(): void
-
-  addEmployee(employee: string): void {
-    this.employees.push(employee)
-  }
-
-  printEmployeeInfo(): void {
-    console.log(this.employees.length)
-    console.log(this.employees)
+user1 = {
+  name: 'Tiago',
+  age: 28,
+  greet(phrase: string) {
+    console.log(phrase)
   }
 }
 
-class AccountingDepartment extends Department {
-  private lastReport: string
-  private static instance: AccountingDepartment
-
-  private constructor(id: string, private reports: string[]) {
-    super(id, 'Accounting Department')
-    this.lastReport = reports[0]
-  }
-
-  static getInstance() {
-    if (AccountingDepartment.instance) return this.instance
-    this.instance = new AccountingDepartment('d2', [])
-    return this.instance
-  }
-
-  addReport(text: string) {
-    this.reports.push(text)
-    this.lastReport = text
-  }
-
-  getReport() {
-    console.log(this.reports)
-  }
-
-  addEmployee(name: string) {
-    if (name === 'Mano') return
-    this.employees.push(name)
-  }
-
-  describe(): void {
-    console.log(`Accounting Department - ID: ${this.id}`)
-  }
-
-  get mostRecentReport() {
-    if (this.lastReport) {
-      return this.lastReport
-    }
-    throw new Error('No Report Found')
-  }
-  set mostRecentReport(report: string) {
-    if (!report) throw new Error('Please provide a valid report')
-    this.addReport(report)
-  }
-}
-
-class ITDepartment extends Department {
-  constructor(id: string, public admins: string[]) {
-    super(id, 'IT')
-  }
-
-  describe(): void {
-    console.log(`IT Department - ID: ${this.id}`)
-  }
-}
-
-const accounting = AccountingDepartment.getInstance()
-const accounting2 = AccountingDepartment.getInstance()
-accounting.describe()
-
-const it = new ITDepartment('d1', ['Mano'])
-it.describe()
-console.log(accounting, accounting2)
+user1.greet('Olá')
